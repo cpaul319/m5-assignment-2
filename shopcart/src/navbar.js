a4772152b84ccfe051dcf53e12d3288d3700c7b7
wu-bootcamp <34114230+cpaul319@users.noreply.github.com>
2022-01-22 16:02:45 -0500

    second commit

--- shopcart/src/navbar.js
+++ shopcart/src/navbar.js
@@ -1,16 +1,41 @@
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {Navbar,NavbarBrand,Nav} from 'reactstrap';
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { faShoppingCart, faRegistered } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import DisplayProducts from './displayProducts';
+import Cart from './cart';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 
-export default function NavBar() {
+export default function NavBar(props) {
     return (
-        {},
-        <Navbar color="primary" light expand="md">
-            <NavbarBrand>Shop to React</NavbarBrand>
-            <Nav>
-            </Nav>
-            <FontAwesomeIcon icon={faShoppingCart} /> 0 items
-</Navbar>
+        <Router>
+            <div className="navbar p-5 bg-info">
+                <h1>
+                    <Link to="/" className="text-decoration-none text-white">
+                    <span className="px-2">Shop 2</span> 
+                    <FontAwesomeIcon icon={faRegistered} className="fas fa-lg text-white"/>eact</Link>
+                </h1>
+                
+                <p className="text-white">
+                <Link to="/showcart">
+                    <FontAwesomeIcon icon={faShoppingCart} className="fas fa-lg mx-3 text-white"/>
+                </Link>
+                <span className="font-weight-bold text-white">{props.totalValue}</span> items</p>
+            </div>
+            
+            <Switch>
+                <Route exact path="/">
+                <DisplayProducts 
+                    products={props.prods}
+                    onIncrement={props.handleIncrement}
+                    onDecrement={props.handleDecrement}
+                />
+                </Route>
+                <Route path="/showcart">
+                    <Cart prods={props.prods}/>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
-
--- shopcart/src/products.js
+++ shopcart/src/products.js
@@ -1,6 +1,6 @@
 export const products = [
-  { id: 1, name: 'cologne', image: './products/cologne.jpg', desc: 'Unisex Cologne', value: 0, ratings: '4' },
-  { id: 2, name: 'iwatch', image: './products/iwatch.jpg', desc: 'Apple iWatch', value: 0, ratings: '3.5' },
-  { id: 3, name: 'mug', image: './products/mug.jpg', desc: 'Unique Mug', value: 0, ratings: '5' },
-  { id: 4, name: 'wallet', image: './products/wallet.jpg', desc: 'Mens Wallet', value: 0, ratings: '4.5' }
+    { id: 1, image: './products/cologne.jpg', desc: 'Unisex Cologne', ratings: '4', value: 0 },
+    { id: 2, image: './products/iwatch.jpg', desc: 'Apple iWatch', ratings: '3.5', value: 0 },
+    { id: 3, image: './products/mug.jpg', desc: 'Unique Mug', ratings: '5', value: 0 },
+    { id: 4, image: './products/wallet.jpg', desc: 'Mens Wallet', ratings: '4.5', value: 0 }
 ]
